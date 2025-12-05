dfc2fd61ea0aa155a28ef2ec59511a9bf14d6d16
Jimboy G. Llagono <GSTJLlagano@robinsonsbank.com.ph>
2025-12-05 09:06:11 +0800

    commit

--- M2_Activity3/src/m2/exer1/Car.java
+++ M2_Activity3/src/m2/exer1/Car.java
@@ -4,19 +4,58 @@
 package m2.exer1;
 
 public class Car {
-
-	String Name;
-	String plateNum;
-	double price;
 	
+	private String carType;
+	private String color;
+	private double price;
 	
-	void travel() {
-		System.out.println("I am driving my car to travel");
+	// No Arguments class Constructor
+	public Car() {
+		
+		
 	}
 	
-	public Car(String Name, String plateNum, double price) {
-		this.Name = Name;
-		this.plateNum = plateNum;
+	// With Parameters class Constructor
+	public Car(String carType, String color, double price) {
+		
+		this.carType = carType;
+		this.color = color;
+		this.price = price;
+		
+	}
+	
+	// method showInfo
+	void showInfo() {
+		System.out.println("============= Car Information ============");
+		System.out.println("Car Type: "+this.carType + "\nCar Color: "+this.color+"\nCar Price: "+this.price);
+		System.out.println("==========================================");
+		
+	}
+	
+	// getters and setters
+	public String getCarType() {
+		return carType;
+	}
+
+
+	public void setCarType(String carType) {
+		this.carType = carType;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
 		this.price = price;
 	}
+	
 }
