e4b7e1a98356ef0deba773f51ecf08b52e4955ec
Jimboy G. Llagono <GSTJLlagano@robinsonsbank.com.ph>
2026-01-16 13:24:08 +0800

    M4_Activity9

---
+++ src/m4/act9/exception/InsufficientFundsException.java
@@ -0,0 +1,9 @@
+package m4.act9.exception;
+
+public class InsufficientFundsException extends Exception {
+	
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+    
+}
---
+++ src/m4/act9/util/TestOperation.java
@@ -0,0 +1,8 @@
+package m4.act9.util;
+
+@FunctionalInterface
+public interface TestOperation {
+	
+	void execute() throws Exception;
+	
+}
