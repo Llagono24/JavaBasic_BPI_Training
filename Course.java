744cbfaa3e608da4e0ac822234932c4865d08ca5
Jimboy G. Llagono <GSTJLlagano@robinsonsbank.com.ph>
2026-01-27 13:52:25 +0800

    M5_Activity3

---
+++ Course.java
@@ -0,0 +1,30 @@
+package com.bpi.M5_Activity3;
+
+public class Course {
+	
+	private Integer studentId;
+	private String name;
+	private Integer grade;
+	
+	public String getName() {
+		return name;
+	}
+	public Integer getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getGrade() {
+		return grade;
+	}
+	public void setGrade(Integer grade) {
+		this.grade = grade;
+	}
+	
+	
+
+}
---
+++ Student.java
@@ -0,0 +1,34 @@
+package com.bpi.M5_Activity3;
+
+public class Student {
+	
+	private String name;
+	private Integer age;
+	private String email;
+	
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Integer getAge() {
+		return this.age;
+	}
+	
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
