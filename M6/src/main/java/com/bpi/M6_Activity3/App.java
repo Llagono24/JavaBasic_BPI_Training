ea2f4d763ee014b1b1f312e2e0547fe575700649
Jimboy G. Llagono <GSTJLlagano@robinsonsbank.com.ph>
2026-01-28 17:22:02 +0800

    M6_Activity4

--- M6/src/main/java/com/bpi/M6_Activity3/App.java
+++ M6/src/main/java/com/bpi/M6_Activity3/App.java
@@ -12,7 +12,7 @@ public class App {
 		   EntityManager em = EntityManagerUtil.getInstance().createEntityManager();
 	    	
 	    	try {
-	    		persistOneToMany(em);
+	    		m6Activity4Solution(em);
 	    		
 	    	} finally {
 	    		EntityManagerUtil.getInstance().closeEntityManager(em);
@@ -51,6 +51,46 @@ public class App {
 			e.printStackTrace();
 		}
 	}
+	
+	//M6 Activity 4 
+	static void m6Activity4Solution(EntityManager em) {
+		
+		em.getTransaction().begin();
+		
+		// 1. create Student object, assign values
+		Student newStudent = new Student();
+		newStudent.setName("Jimboy Llagono");
+		newStudent.setAge(26);
+		newStudent.setEmail("llagonojimboy@gmail.com");
+		// 2. attach transient student object to persistence context
+		em.persist(newStudent);
+		// 3. call flush()	
+		em.flush();
+		// 4. detach the managed newStudent from the persistence context
+		em.detach(newStudent);
+
+		// 5. print "is newStudent inside the persistence context: " + call contains()
+		System.out.println("is newStudent inside the persistence context: " + em.contains(newStudent));
+
+		// 6. reattach the detached newStudent
+		newStudent = em.merge(newStudent);
+		// 7. update newStudent, change some values like age or email
+		newStudent.setAge(26);
+		newStudent.setEmail("juan.delacruz@gmail.com");
+		// 8. call flush()
+		em.flush();
+		// 9. print "is newStudent inside the persistence context: " + call contains()
+		System.out.println("is newStudent inside the persistence context: " + em.contains(newStudent));
+		// 11. mark managed newStudent for deletion
+		em.remove(newStudent);
+		// 12. call flush()
+		em.flush();
+		// 13. print "is newStudent inside the persistence context: " + call contains()
+		System.out.println("is newStudent inside the persistence context: " + em.contains(newStudent));
+		
+		em.getTransaction().commit();
+	}
+
 }
 
 
