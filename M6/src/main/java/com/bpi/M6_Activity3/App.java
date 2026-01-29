a1decef35a822e591b1098c5b4911bfbb3c574cd
Jimboy G. Llagono <GSTJLlagano@robinsonsbank.com.ph>
2026-01-29 16:42:44 +0800

    M6_Activity5

--- M6/src/main/java/com/bpi/M6_Activity3/App.java
+++ M6/src/main/java/com/bpi/M6_Activity3/App.java
@@ -4,15 +4,21 @@ import com.bpi.M6_Activity3.model.Student;
 import com.bpi.M6_Activity3.model.Course;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
 
 public class App {
 
 	   public static void main(String[] args) {
 		   
+
+		   
 		   EntityManager em = EntityManagerUtil.getInstance().createEntityManager();
 	    	
 	    	try {
-	    		m6Activity4Solution(em);
+	    		printAllStudentNames(em);
+	    		countCoursesByStudentId(em, 1L);
+	    		countStudentsByAgeGreaterThan(em, 25);
 	    		
 	    	} finally {
 	    		EntityManagerUtil.getInstance().closeEntityManager(em);
@@ -90,6 +96,89 @@ public class App {
 		
 		em.getTransaction().commit();
 	}
+	
+//	For lecture demo 
+//	static void selectAllStudent(EntityManager em) {
+//		
+//		em.getTransaction().begin();
+//		
+//		String jpql = "Select s FROM Student s";
+//		TypedQuery<Student> query = em.createQuery(jpql, Student.class);
+//		List<Student> students = query.getResultList();
+//		
+//		//print student names
+//		students.forEach(student -> System.out.println(student.getName()));
+//		
+//		em.getTransaction().commit();
+//		
+//	}
+//	
+//	static void selectStudentByName(EntityManager em, String name) {
+//		
+//		em.getTransaction().begin();
+//		
+//		String jpql = "Select s FROM Student s where s.name = :name";
+//		TypedQuery<Student> query = em.createQuery(jpql, Student.class);
+//		query.setParameter("name", name);
+//		List<Student> students = query.getResultList();
+//		
+//		//print student names
+//		students.forEach(student -> System.out.println(student.getName()));
+//		
+//		em.getTransaction().commit();
+//		
+//	}
+	
+	
+	// For Activity 5 Start
+	static void printAllStudentNames(EntityManager em) {
+		em.getTransaction().begin();
+		
+		String jpql = "Select s FROM Student s";
+		TypedQuery<Student> query = em.createQuery(jpql, Student.class);
+		List<Student> students = query.getResultList();
+		
+		//print student names
+		System.out.println("Result:");
+		students.forEach(student -> System.out.println(student.getName()));
+		
+		em.getTransaction().commit();
+		
+	}
+
+
+	static void countCoursesByStudentId(EntityManager em, Long id) {
+		em.getTransaction().begin();
+		
+		String jpql = "Select count(c) FROM Course c JOIN c.student s where s.id = :studId";
+		TypedQuery<Long> query = em.createQuery(jpql, Long.class);
+		query.setParameter("studId", id);
+		Long countCourse = (Long) query.getSingleResult();
+		
+		//print course count result
+		System.out.println("Result:");
+		System.out.println(countCourse); 
+		
+		em.getTransaction().commit();
+	}
+
+	
+	static void countStudentsByAgeGreaterThan(EntityManager em, int age) {
+		em.getTransaction().begin();
+		
+		String jpql = "Select count(s) FROM Student s where s.age > :studAge";
+		TypedQuery<Long> query = em.createQuery(jpql, Long.class);
+		query.setParameter("studAge", age);
+		Long countStudents = (Long)query.getSingleResult();
+		
+		//print student age greater than
+		System.out.println("Result:");
+		System.out.println(countStudents); 
+		
+		em.getTransaction().commit();
+	}
+	// For Activity 5 End
+
 
 }
 
